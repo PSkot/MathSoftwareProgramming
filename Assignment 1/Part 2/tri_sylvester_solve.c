a46add07830489551cf245c6ea36d8a7dccaea73
PSkot <peter.skoet@gmail.com>
2018-10-22 22:36:25 +0200

    Update sylvester equation

--- Assignment 1/Part 2/tri_sylvester_solve.c
+++ Assignment 1/Part 2/tri_sylvester_solve.c
@@ -2,31 +2,51 @@
 #include <math.h>
 #include "matrix.h"
 
-double* fwsubst(
-  unsigned long n,
-  double alpha,
-  double **R,  /* two-dimensional array, row-major */
-  double *b    /* one-dimensional array */
-);
+double* fwsubst(unsigned long n, double alpha, double **R, double *b);
 
-int tri_sylvester_solve(const matrix_t *R, matrix_t *C) {
+int tri_sylvester_solve(const matrix_t *R, matrix_t *C){
 
+  //Ensure correct dimensions and no NULL pointers
+  if(
+  (R == NULL) ||
+  (C == NULL) ||
+  !(C->n == C->m && R->n == R->m && C->n == R->n) ||
+  (C->n <= 0)
+  )
+  {
+    return -2;
+  }
+
+  //Run step 2 for the first row of X (overwriting C)
   C->A[0] = fwsubst(C->n, R->A[0][0], R->A, C->A[0]);
 
+  //Apply step 1 and 2 for remaining X's (overwriting C)
   for(size_t k = 1; k < C->n; ++k){
-    double* sum = (double*)malloc(C->n*sizeof(double));
+    double* sum = malloc(C->n*sizeof(double));
     for(size_t i = 0; i <= k - 1; ++i){
       for(size_t j = 0; j < C->n; ++j){
       sum[j] += C->A[i][j]*R->A[i][k];
       }
-      }
+    }
       for(size_t j = 0; j < C->n; ++j){
+        //Step 1
         C->A[k][j] -= sum[j];
       }
+        //Step 2
         C->A[k] = fwsubst(C->n, R->A[k][k], R->A, C->A[k]);
-      }
-        return 0;
+  }
+
+  //Check for numerical failure, return -1 if failure
+  for(size_t i = 0; i < C->n; ++i){
+    for(size_t j = 0; j < C->n; ++j){
+      if(!isfinite(C->A[i][j]))
+        return -1;
     }
+  }
+
+    //Success
+    return 0;
+}
 
 double* fwsubst(
   unsigned long n,
@@ -38,7 +58,7 @@ double* fwsubst(
     //Calculate first solution element x (overwriting b)
     b[0] /= alpha + R[0][0];
 
-    //Calculate the remaining x's (overwriting b)
+    //Calculate the remaining x's through forward substitution (overwriting b)
     for(size_t k = 1; k < n; ++k){
       double sum = 0.0;
       for(size_t i = 0; i <= k - 1; ++i){
@@ -46,13 +66,6 @@ double* fwsubst(
       }
       b[k] = (b[k] - sum)/(alpha + R[k][k]);
     }
-
-    //Check for numerical failure, return -1 if failure
-      /*for(size_t i = 0; i < n; ++i)
-      {
-        if(!isfinite(b[i]))
-          return -1;
-      }*/
 
     //Success
     return b;
