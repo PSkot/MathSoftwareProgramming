7910897f796b50fc61443db8d2db2d69bbb8b8c9
PSkot <36443719+PSkot@users.noreply.github.com>
2018-10-22 18:28:30 +0200

    Update Assignment 1/Part 2/tri_sylvester_solve.c

--- Assignment 1/Part 2/tri_sylvester_solve.c
+++ Assignment 1/Part 2/tri_sylvester_solve.c
@@ -2,7 +2,7 @@
 #include <math.h>
 #include "matrix.h"
 
-int fwsubst(
+double* fwsubst(
   unsigned long n,
   double alpha,
   double **R,  /* two-dimensional array, row-major */
@@ -11,23 +11,24 @@ int fwsubst(
 
 int tri_sylvester_solve(const matrix_t *R, matrix_t *C) {
 
-  double* tmp = fwsubst(C->n, R->A[0][0], R->A, C->A[0]);
-  memcpy(tmp, )
+  C->A[0] = fwsubst(C->n, R->A[0][0], R->A, C->A[0]);
 
   for(size_t k = 1; k < C->n; ++k){
-    double* sum = malloc(C->n*sizeof(double));
+    double* sum = (double*)malloc(C->n*sizeof(double));
     for(size_t i = 0; i <= k - 1; ++i){
       for(size_t j = 0; j < C->n; ++j){
       sum[j] += C->A[i][j]*R->A[i][k];
       }
       }
-        C->A[k] = C->A[k] - sum;
+      for(size_t j = 0; j < C->n; ++j){
+        C->A[k][j] -= sum[j];
+      }
         C->A[k] = fwsubst(C->n, R->A[k][k], R->A, C->A[k]);
       }
         return 0;
     }
 
-int fwsubst(
+double* fwsubst(
   unsigned long n,
   double alpha,
   double **R,  /* two-dimensional array, row-major */
@@ -47,12 +48,12 @@ int fwsubst(
     }
 
     //Check for numerical failure, return -1 if failure
-      for(size_t i = 0; i < n; ++i)
+      /*for(size_t i = 0; i < n; ++i)
       {
         if(!isfinite(b[i]))
           return -1;
-      }
+      }*/
 
     //Success
-    return 0;
+    return b;
 }
